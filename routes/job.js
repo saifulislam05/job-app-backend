dd6f6e77a415d354ba1e8f27527db7ea41030a41
saifulislam05 <saiful.bijulia@gmail.com>
2024-03-01 08:15:01 +0530

    new init

--- routes/job.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import { getJob,createJob } from "../controllers/job.js";
-const router = express.Router();
-
-router.post("",createJob);
-router.get("", getJob);
-
-export { router };
