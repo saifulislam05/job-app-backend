379bbbc83afbc7581c646f7d83c049e3a377c440
saifulislam05 <saiful.bijulia@gmail.com>
2024-03-01 19:33:44 +0530

    Atlas cluster connection done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import express from "express";
 import router from "./routes/jobs.js";
 import mongoose from "mongoose";
+
 // creating server
 const app = express();
 // body perser Middleware
@@ -8,12 +10,16 @@ app.use(express.json());
 
 // connection with Mongo Database
 mongoose
-  .connect(`mongodb://localhost:27017/job-app`)
+  //   .connect(`mongodb://localhost:27017/job-app`)
+  // 
+  .connect(
+    `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.xo4ltbz.mongodb.net/`
+  )
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log("Database connection Failed", err));
 
 // Jobs Endpoint
-app.use("/jobs", router)
+app.use("/jobs", router);
 
 // listen on port
 app.listen("10000", () => console.log("App is running on port 10000"));
