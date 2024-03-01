415147c41df9177e22941c6748ffff49046c63b7
saifulislam05 <saiful.bijulia@gmail.com>
2024-03-01 17:03:51 +0530

    job Deleteing endpoint done

--- controller/jobs.js
+++ controller/jobs.js
@@ -1,54 +1,70 @@
 import jobsModel from "../model/jobs.js";
 
 const getJobs = async (req, res) => {
-  try {
-    const jobs = await jobsModel.find();
+    try {
+        const jobs = await jobsModel.find();
 
-    res.status(200).json({
-      success: true,
-      message: "Fetched all the data",
-      results: jobs,
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      message: `not Found`,
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "Fetched all the data",
+            results: jobs,
+        });
+    } catch (error) {
+        res.status(404).json({
+            success: false,
+            message: `not Found`,
+        });
+    }
 };
 
 const createJob = async (req, res) => {
-  try {
-    const newJob = new jobsModel(req.body);
-    const newlyAddedJob = await newJob.save();
-    res.status(201).json({
-      success: true,
-      message: "job inserted Successfully",
-      result: newlyAddedJob,
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: `something went wrong ${error}`,
-    });
-  }
+    try {
+        const newJob = new jobsModel(req.body);
+        const newlyAddedJob = await newJob.save();
+        res.status(201).json({
+            success: true,
+            message: "job inserted Successfully",
+            result: newlyAddedJob,
+        });
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: `something went wrong ${error}`,
+        });
+    }
 };
 const updateJob = async (req, res) => {
-  try {
-    const jobId = req.body._id;
-    const newJob = new jobsModel(req.body);
-    await jobsModel.findByIdAndUpdate(jobId, newJob);
+    try {
+        const jobId = req.body._id;
+        const newJob = new jobsModel(req.body);
+        await jobsModel.findByIdAndUpdate(jobId, newJob);
 
-    res.status(200).json({
-      success: true,
-      message: "job updated Successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: `error : ${error.message}`,
-    });
-  }
+        res.status(200).json({
+            success: true,
+            message: "job updated Successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: `error : ${error.message}`,
+        });
+    }
 };
 
-export { getJobs, createJob, updateJob };
+const deleteJob = async (req, res) => {
+    try {
+        const jobId = req.body._id;
+        await jobsModel.findByIdAndDelete(jobId);
+        res.status(200).json({
+            success: true,
+            message: "job Deleted Successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: `error : ${error.message}`,
+        });
+    }
+};
+
+export { getJobs, createJob, updateJob, deleteJob };
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getJobs, createJob, updateJob } from "../controller/jobs.js";
+import { getJobs, createJob, updateJob, deleteJob } from "../controller/jobs.js";
 
 const router = express.Router();
 
 router.get("", getJobs);
 router.post("", createJob);
 router.patch("", updateJob);
+router.delete("", deleteJob);
 
 export default router;
